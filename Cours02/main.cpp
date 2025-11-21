e885c425437bbfdc384e647a6d526d29d21ccd8a
minicube707 <florentm.contact@gmail.com>
2025-11-21 02:57:57 +0100

    Add Cours01 & 02

---
+++ Cours02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    //Commentaire
+    /*
+        BIG COMMENTAIRE
+    */
+
+    //std: utilise la bibliotheque standart
+    //srd::cout: sortie standart (stdout)
+    //std::cerr: sorite erreur   (stderr)
+    //std::clog: journalisation
+    //std::endl: retour a la ligne + flush (vide le tampon) (end line)
+
+    std::cout << "Hello World!" << std::endl;
+    return (0);
+}
